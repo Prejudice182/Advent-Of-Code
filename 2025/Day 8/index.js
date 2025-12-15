acacb00e41bcf746d2118b04c3b98d4a9b20bbd6
prejudice182 <redpaulkenny@gmail.com>
2025-12-15 22:34:14 +0000

    Refactor day 8

--- 2025/Day 8/index.js
+++ 2025/Day 8/index.js
@@ -1,44 +1,27 @@
-const { start } = require('node:repl')
-
 const input = require('node:fs').readFileSync('input.txt', 'utf8')
 const test = require('node:fs').readFileSync('test.txt', 'utf8')
 
-class DefaultDict {
-  constructor(defaultInit) {
-    return new Proxy({}, {
-      get: (target, name) => name in target ?
-        target[name] :
-        (target[name] = typeof defaultInit === 'function' ?
-          new defaultInit().valueOf() :
-          defaultInit)
-    })
-  }
-}
+const euclideanDistance = (a, b) => Math.hypot(...a.map((v, i) => v - b[i]))
 
-const euclideanDistance = (a, b) => Math.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2 + (a[2] - b[2]) ** 2)
-
-const bothParts = (data) => {
+const bothParts = (data, c = 1000) => {
     const junctionBoxes = data.split('\n').map(line => line.split(',').map(Number))
     const distances = []
-    for (const [i, box1] of junctionBoxes.entries()) {
-        for (const [j, box2] of junctionBoxes.entries()) {
-            const distance = euclideanDistance(box1, box2)
-            if (i > j) distances.push([distance, i, j])
-        }
-    }
+    for (const [i, box1] of junctionBoxes.entries()) 
+      for (const [j, box2] of junctionBoxes.entries()) 
+        if (i > j) distances.push([euclideanDistance(box1, box2), i, j])
     distances.sort((a, b) => a[0] - b[0])
-    const unionFind = Array(junctionBoxes.length).fill(0).map((_, i) => i)
+
+    const uf = [...Array(junctionBoxes.length).keys()]
     const find = (x) => {
-        if (x === unionFind[x]) return x
-        unionFind[x] = find(unionFind[x])
-        return unionFind[x]
+        if (x === uf[x]) return x
+        uf[x] = find(uf[x])
+        return uf[x]
     }
-    const mix = (x, y) => unionFind[find(x)] = find(y)
+    const union = (x, y) => uf[find(x)] = find(y)
     let connections = 0
-    for (const [total, [d, i, j]] of distances.entries()) {
-        if (total === 1000) {
-            const startAtZero = new Map()
-            for (let k = 0; k < junctionBoxes.length; k++) startAtZero.set(k, 0)
+    for (const [total, [_, i, j]] of distances.entries()) {
+        if (total === c) {
+            const startAtZero = new Map([...junctionBoxes.keys()].map(k => [k, 0]))
             for (const x of junctionBoxes.keys()) startAtZero.set(find(x), (startAtZero.get(find(x) ?? 0) + 1))
             const s = [...startAtZero.values()].sort((a, b) => b - a).slice(0, 3).reduce((a, b) => a * b, 1)
             console.log(s)
@@ -46,10 +29,10 @@ const bothParts = (data) => {
         if (find(i) !== find(j)) {
             connections++
             if (connections === junctionBoxes.length - 1) console.log(junctionBoxes[i][0] * junctionBoxes[j][0])
-            mix(i, j)
+            union(i, j)
         }
     }
 }
 
-bothParts(test)
+bothParts(test, 10)
 bothParts(input)
